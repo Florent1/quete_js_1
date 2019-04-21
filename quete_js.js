ac8da53ec0ef6f4f1967a3344b408aebd539f2f8
Florent330 <Florent_Duval_student2019@wilder.school>
2019-04-21 16:43:24 +0200

    Create quete_js.js

---
+++ quete_js.js
@@ -0,0 +1,6 @@
+
+const movieName = 'Ready Player One';
+const releaseYear = 2018;
+const director = 'Steven Spielberg ';
+
+alert(movieName + ' est sorti en ' + releaseYear + ' et a été réalisé par ' + director + '.');
